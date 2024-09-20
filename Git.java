bd1bafc60288f25f919190d95290bdfc1ba4ec4b
Ace <jschneider1@hwemail.com>
2024-09-20 12:02:18 -0700

    Milestone #1: Added Git() to Git.java!
    Creates a public git directory containing an objects directory and an index file.

--- Git.java
+++ Git.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Git {
     public Git() {
-        
+        boolean newDirectoriesOrFilesCreated = false; // true if any new directories and/or files were created w/ this constructor method.
+        if (new File("git").mkdir()) {
+            newDirectoriesOrFilesCreated = true;
+        } if (new File("git/objects").mkdir()) {
+            newDirectoriesOrFilesCreated = true;
+        }
+        File indexFile = new File("git/index");
+        try {
+            if (indexFile.createNewFile()) {
+                newDirectoriesOrFilesCreated = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (newDirectoriesOrFilesCreated) {
+            System.out.println("Git Repository successfully created!");
+        } else {
+            System.out.println("Git Repository already exists.");
+        }
     }
 }
---
+++ GitTester.java
@@ -0,0 +1,10 @@
+public class GitTester {
+    public static void main(String[] args) {
+        System.out.println("Testing Git class...");
+
+        System.out.println("\nTesting constructor...");
+        System.out.println("Test 1: standard test (none of the directories nor files exist before)...");
+        Git testGit = new Git();
+        
+    }
+}
